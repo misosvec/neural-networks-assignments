9ff6dcb971390b8119c3eadf2234a0517ddfe121
Michal Švec <misosvec01@gmail.com>
2025-04-18 10:17:30 +0200

    helper functions

--- multilayer-perceptron-classifier/utils.py
+++ multilayer-perceptron-classifier/utils.py
@@ -8,3 +8,16 @@ def onehot_encode(x):
         label_int = label_to_int[label]
         onehot[i, label_int] = 1
     return onehot
+
+def add_bias(X):
+    if X.ndim == 1:
+        return np.concatenate((X, [1]))
+    else:
+        pad = np.ones((1, X.shape[1]))
+        return np.concatenate((X, pad), axis=0)
+
+def compute_accuracy(y_true, y_pred):
+    true_labels = np.argmax(y_true, axis=0)
+    pred_labels = np.argmax(y_pred, axis=0)
+    accuracy = np.mean(true_labels == pred_labels)
+    return accuracy
