c59b5928ecd0187dbd7c225cfa9d1f947c027b27
Michal Švec <misosvec01@gmail.com>
2025-05-03 10:49:33 +0200

    fixed point, limit cycle, most frequent states

--- hopfield-network/hopfield.py
+++ hopfield-network/hopfield.py
@@ -23,14 +23,33 @@ class HopfieldNetwork:
                     e += self.W[i][j]*s[i]*s[j]
         return -1/2 * e
 
-    def run_sync(self, s, eps = 20):
+    def run_sync(self, s):
         states = [s.copy()]
         energies = [self.energy(s)]
-        
-        for ep in range(eps):
+        is_fixed_point = False
+        while True:
             s = np.dot(self.W, s)
             s = np.where(s >= 0, 1, -1)
 
             states.append(s.copy()) 
             energies.append(self.energy(s))
-        return states, energies
+            if self._is_fixed_point(states):
+                is_fixed_point = True
+                break
+            if self._is_limit_cycle(states):
+                break
+        return states, energies, is_fixed_point
+    
+    def _is_fixed_point(self, states):
+        if len(states) < 2:
+            return False
+        return np.array_equal(states[-1], states[-2])
+    
+    def _is_limit_cycle(self, states):
+        if len(states) < 3:
+            return False
+        last_state = states[-1]
+        for k in range(len(states) - 2):
+            if np.array_equal(last_state, states[k]):
+                return True
+        return False
